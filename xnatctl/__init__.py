ec5a56160d8136e4d7684cf55b98efb3f9c11203
Ricky Wong <ricky.wong@camh.ca>
2026-03-25 07:48:57 -0400

    chore(release): bump version to 0.2.6

--- xnatctl/__init__.py
+++ xnatctl/__init__.py
@@ -8,7 +8,7 @@ neuroimaging data servers, supporting common workflows like:
 - Administrative operations (catalogs, users, renaming)
 """
 
-__version__ = "0.2.5"
+__version__ = "0.2.6"
 __author__ = "Ricky Wong"
 
 from xnatctl.core.client import XNATClient
